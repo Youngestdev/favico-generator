ae242dd36d027c1ac9d778c3578dbc227d3e9010
Ajala Abdulsamii <kgasta@gmail.com>
2017-12-19 04:11:50 +0100

    reformatted

--- index.js
+++ index.js
@@ -5,24 +5,25 @@
 const im = require('imagemagick');
 const fs = require('fs');
 const argv = require('yargs')
-    .usage('Usage: $0   option filename \n e.g $0 -f image.png')
-    .alias('f', 'file')
-    .nargs('f', 1)
-    .describe('f', 'Loads file to be converted to favico')
-    .demandOption(['f'])
-    .help('h')
-    .alias('h', 'help')
-    .epilog('Copyright   Abdul 2017')
-    .argv;
+  .usage('Usage: $0 option filename \n e.g $0 -f image.png')
+  .alias('f', 'file')
+  .nargs('f', 1)
+  .describe('f', 'Loads file to be converted to favico')
+  .demandOption(['f'])
+  .help('h')
+  .alias('h', 'help')
+  .epilog('Copyright   Abdul 2017')
+  .argv;
 
 const s = fs.createReadStream(argv.file);
 
-const convert = s.on('data', (buf) => im.convert([argv.file, '-resize', '160x160', 'favico.ico'], 
-function(err, stdout){
-  if (err) throw err;
-//   console.log('stdout:',  stdout);
-})
-);
+const convert = s.on('data', (buf) => im.convert([
+  argv.file, '-resize', '160x160', 'favico.ico'
+], function (err, stdout) {
+  if (err) 
+    throw err;
+    //   console.log('stdout:',  stdout);
+  }
+));
 
-s.on('end', () => console.log('Success, File saved as "favico.ico in ' +process.cwd()));
-
+s.on('end', () => console.log('Success, File saved as "favico.ico in ' + process.cwd()));
